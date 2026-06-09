0706dda2411657b0feb78a9820c25de9b8576baf
ririndeismillia <ririndeismillia@gmail.com>
2026-06-09 11:20:12 +0700

    void search

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -57,4 +57,22 @@ public:
         }
     }
 
-    
+    // this function searches the current node of the specified node
+    // as well as the current node of its parent
+    void search(string element, node *&parent, node *&currentnode)
+    {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+    }
+
+};
